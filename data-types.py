b1f3a2903443dbf3e695058ff4d7d2068ab192d5
alopay <121797602+alopay@users.noreply.github.com>
2023-01-05 13:30:19 +0100

    data type introduction

---
+++ data-types.py
@@ -0,0 +1,29 @@
+# this is a single line comment
+
+print("Hello World")  # print our message to the terminal/console
+
+# Numbers, String and Booleans
+
+# Numbers e.g int, float
+# 1, 2, 3, 5 are examples of int
+# 3.2, 0.1, 1.9 are examples of float
+
+print(2 * 2)  # result will always be an integer
+print(2.1 * 0.2)  # result will always be in decimal/float
+print(2 * 0.5)  # result will always be a float/decimal
+print(4 / 0.5)  # result will always be a float/decimal
+
+# Strings are always wrapped in quotes
+
+print("Hello Mr. Alopay")
+print("how are you doing today?")
+print('2 * 0.02')
+
+# Boolean Values are always in 2 states. True/False
+is_python = True  # True is another way of saying yes
+is_js = False  # False is another way of saying no
+print(is_python)
+
+# type() is used to detect a data type
+
+print(type(is_python))
