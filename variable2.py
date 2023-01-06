964a4b17d0be240a6284dde1d902cc6b84287add
alopay <121797602+alopay@users.noreply.github.com>
2023-01-06 12:35:42 +0100

    variable manipulation in python

---
+++ variable2.py
@@ -0,0 +1,25 @@
+# dynamic variable type in python
+# manipulating varibale data type
+
+m = 1
+# 'type' to check for data types
+
+print(m)
+print(type(m))
+
+m = "Holla" # this is a string
+
+print(m)
+print(type(m))
+
+m = 0.01 # this is a float/decimal
+print(m)
+print(type(m))
+
+m = True # this is a boolean
+print(m)
+print(type(m))
+
+n = m
+print(n)
+print(type(n))
