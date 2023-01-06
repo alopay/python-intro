190378fdd1f00f7b195f29259cb2403326d09ce3
alopay <121797602+alopay@users.noreply.github.com>
2023-01-06 12:09:26 +0100

    introduction to variables in python

---
+++ variable1.py
@@ -0,0 +1,29 @@
+# In this tutorial our primary focus will be on variables
+# and a further introduction into data types
+number = 3000000  # define a variable
+# 'number' is a variable name, '=' is an assignment operator, '300000' is the actual value
+print(number)
+
+greetings = "Hello User"
+# 'greetings' is a variable name, '=' is an assignement operator, 'Hello User' is the actual value
+print(greetings)
+
+balance = 200.9
+print(balance)
+# "Hello World"
+# print("Hello World")
+
+sentence = "Noodles: You can use either traditional lasagna noodles or no-boil lasagna noodles for your lasagna"
+
+n = -5
+m = -5
+print(n is m)  # 'is' is a comaprison operator and a builtin python keyword
+# True and false is a boolean data type.
+# 'True' means 'yes' and 'False' means 'no'
+
+# print(sentence)
+
+name = "Alopay"
+print(name)
+surname = name
+print(surname)
