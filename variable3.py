7172fae482fdf93bb969c6195a514c3324999595
alopay <121797602+alopay@users.noreply.github.com>
2023-01-06 13:09:49 +0100

    naming variables

---
+++ variable3.py
@@ -0,0 +1,28 @@
+# naming variables
+
+number = 2000  # Variables can be in lower case
+age = 39
+name = "Hakeem"
+
+NAME = "test me"  # Variables can be in upper case
+print(NAME)
+
+isAway = True  # Variables can be in both upper and lower case
+print(isAway)
+
+_is_home_ = False  # variables can also take underscore
+print(_is_home_)
+
+# is-foreigner = False # variables cannot take hyphen because '-' is represented as minus in python
+
+variable1 = "test"  # variables can end with numbers
+variable_2 = "another test"  # variables can end with both numbers and underscore
+var2iable = "Third Test"  # variables can have numbers in between
+# 2variable = "this is another example" # variables cannot start with int
+
+# print = 1 # variables can not be any of python keyword arguments
+
+# Reserved Words (Keywords) in python
+
+
+ 
