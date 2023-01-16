d704595db97971e2e3913643891ce50aa33780c1
alopay <phikatravel@gmail.com>
2023-01-16 12:13:44 +0000

    python arithmetic operator

---
+++ arithmetic.py
@@ -0,0 +1,52 @@
+# Operators and expressions
+# operators are special symbols that designate that some sort of computation should be performed
+
+# "=" is used as an assignment operator
+a = 10
+b = 20
+
+# "+" is used to add to or more values together
+print(a + b)
+
+total = b - a
+print(total)
+
+# -a changed the value of a (10) to -10
+print(-a)
+
+# "*" is used for multiplication
+multiple = a * b
+
+print(multiple)
+
+c = 7
+d = 3
+
+# "/" is used to represent division
+dividend = c / d
+
+print(dividend)
+
+# "//" is used for floor division
+floor = c // d
+print(floor)
+
+# a = 10, b = 20, c = 7, d = 3
+
+# BODMAS - note: python uses bodmas for calculation
+calculation = c * a + b / d
+print(calculation)
+
+# "%" is used to get remainder and called the modulo operator
+
+# 7 - 6 = 1
+modulo = c % d
+modulo1 = a % c
+
+print(modulo)
+print(modulo1)
+
+# "**" is used to get the exponent value of a number
+
+square_c = c**2  # c * c
+print(square_c)
