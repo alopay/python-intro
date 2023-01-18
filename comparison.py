701fe05690466cce74b3ad4317ede25550adc00a
alopay <phikatravel@gmail.com>
2023-01-18 10:35:01 +0000

    python comparison operators

---
+++ comparison.py
@@ -0,0 +1,37 @@
+# Comparison operators in python
+
+a = 10
+b = 20
+c = 20
+
+# '=' is used to assign value
+# "==" is a equal to operator. is used to compare values
+# print(a == b)  # a = 10, b = 20, this statement will be False
+# print(c == b)  # c = 20, b = 20, this statement will be True
+
+# "!=" is a not equal to operator. is used to compare values
+# print(a != b)
+# print(c != b)
+
+# "<" is a lesser than operator.
+# print(a < b)
+# print(c < b)
+
+# ">" is a greater than operator
+# print(a > b)
+# print(b > c)
+
+# "<=" Less than or equal to operator
+
+# print(a <= b)
+# print(c <= b)
+
+# ">=" Greater than or equal to operator
+# print(b >= c)
+# print(a >= b)
+
+x = 1.1 + 2.2  #3.3
+print(x)
+
+print(x == 3.3)
+
