aaf56739c6659d2fd649c85f3289b7a186ae2a8b
alopay <phikatravel@gmail.com>
2023-01-20 10:48:30 +0000

    summary of work so fars

---
+++ logic.py
@@ -0,0 +1,20 @@
+# Logical operators
+
+# not or and logical operators
+k = 20
+y = 15
+d = 30
+
+# print(not 20 < 15) # k is not less than y
+# print(20 < 15) # k is less than y
+# print(not d < y)
+
+# 'or' check if one condition is true and return true otherwise, it returns false
+# print(k < d or k < y)
+# print(d < y or d < k)
+
+# 'and' check if all condition is true and return true otherwise, it returns false
+
+print(k < d and k < y)
+print(y < d and y < k)
+print(k < d and d > y)
---
+++ summary.py
@@ -0,0 +1,23 @@
+glass_of_water = 2
+
+# glass_of_water = glass_of_water + 1
+
+glass_of_water += 3 # 5 and 5 is an integer
+
+answer_1 = type(glass_of_water)
+name = 'Bob'
+
+answer_2 = type(name)
+
+balance = 20.2
+print(type(balance))
+str_balance = str(balance)
+print(type(str_balance))
+
+int_balance = int(balance)
+print(type(int_balance))
+
+is_alive = "True"
+str_status = bool(is_alive)
+
+print(str_status)
