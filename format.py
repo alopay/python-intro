31f9c90c52948226fd31fbe610271d561449f4c4
alopay <phikatravel@gmail.com>
2023-01-20 12:00:52 +0000

    format exercise

---
+++ format.py
@@ -0,0 +1,18 @@
+
+# String manipulation format string "f"
+checker = True
+greeting = "Hello"
+name = "Bello"
+year = 1
+
+
+sentence = f"{greeting}, I am {name}. I am in my {year}st Year. This is a {checker} Statement."
+print(sentence)
+
+surname = "Alabi"
+age = 2
+occupation = "Python Developer"
+
+# Hello I am Alopay. I am 2years Old. I am also a Python Developer
+sentence_1 = f"Hello I am {surname}. I am {age}years old. I am also a {occupation}"
+print(sentence_1)
