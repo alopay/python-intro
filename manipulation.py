02137c5fe7384c294c19c74b24062e10e92ca5e7
alopay <phikatravel@gmail.com>
2023-01-20 11:38:58 +0000

    manipulation practice

---
+++ manipulation.py
@@ -0,0 +1,18 @@
+# sentence = "Hello, I am Alopay. And I'm in my 4th year of learning"
+
+# greeting = "Hello, "
+# intro = "I am Alopay. "
+# extra = "And I'm in my "
+# year = 4
+# end = "th year of learning"
+# print(greeting+intro+extra+str(year)+end)
+
+# sentence2 = "Hello, I am Bello. And I'm in my 1st year of learning. This is True"
+checker = True
+greeting = "Hello, "
+intro = "I am Bello. "
+extra = "And I'm in my "
+year = 1
+cont = "st year of learning. "
+end = "This is "
+print(greeting+intro+extra+str(year)+cont+end+str(checker))
