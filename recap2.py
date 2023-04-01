89f7896fc8820f0eb307f644380120a99a441194
alopay <phikatravel@gmail.com>
2023-04-01 11:54:00 +0100

    operator precedences

---
+++ recap2.py
@@ -0,0 +1,33 @@
+# Operator Precedence
+# For Maths
+# Bodmas - Bracket, of, division, multiplication, addition and subtraction
+
+# for Programming
+# BEDMAS - Bracket, Exponent, Division, Multiplication, Addition and subtraction
+
+# print(20 + 4 * 10)
+# print(30 + 40 / 10)
+
+# print(20 * 10 / 4)
+
+# print(2 * 3 ** 4 * 5)
+# print(2 * 3 ** (4 * 5))
+# print((2 * 3) ** (4 * 5))
+
+
+# Augmented Assignment Operators
+a = 10
+b = 5
+c = a * 2 + b
+
+print(c)
+
+# a = a + 2
+# print(a)
+
+a += 4
+print(a)
+
+b *= 3
+print(b)
+
