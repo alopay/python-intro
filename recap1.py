63749181d1a3997c06b3d580680032cba5059dd3
alopay <phikatravel@gmail.com>
2023-04-01 11:29:24 +0100

    recap from past tutorials

---
+++ recap1.py
@@ -0,0 +1,91 @@
+# # integers, string, booleans
+
+
+# # integers - numbers and float(decimal) 1, 13, 17, 2.1 3.3
+
+# fname = "Mr Alopay"
+# school = 'ui'
+
+# print(fname)
+
+# # variables are immutable
+# fname = "Mr Bello"
+# print(fname)
+
+# a = b = c = d = 300
+
+# print(a, b, c, d)
+
+# # 1star wrong variable name
+# name = "Mr alopay"
+# NAME = "MR Alopay"
+# ageYear = 12
+# age_year = 12
+# name1 = "Mr Hakeem"
+# name2age = 10
+
+# Arithmetic Operation
+# addition, subtraction, multiplication, division + - * / 
+
+# floor division //
+# modulo or remainder %
+# exponent or raise to power **
+
+# print(2*4)
+# print(2**4)
+
+age1 = 32
+age2 = 20
+age3 = 20
+
+
+# Comparison Operator
+# equal to operator "==" it returns boolean value
+# print(age3 == age2)
+
+# not equal to operator "!=" it returns boolean value
+
+# print(age3 != age2)
+# print(age1 != age2)
+
+# less than operator '<' it returns boolean value
+
+# print(age1 < age2)
+# print(age3 < age1)
+
+# Greater than operator '>' it returns boolean value
+
+# print(age1 > age3)
+
+# print(3 > 20)
+
+# less than or equal to "<=" it returns boolean value
+
+# print(41 <= 40)
+
+# greater than or equal to ">=" it returns boolean value
+
+# print(39 >= 40)
+
+
+# logical operators 
+# and, or, not
+
+# print(1+2 or 2+3)
+
+# print(1 < 2 and 1 == 2 and 3 == 3 and 20 < 30)
+
+# print(1 < 2) 
+# print(not 1 < 2) # False
+
+# print(1 > 3)
+# print(not 1 > 3)
+
+name = "Ola"
+name2 = "Ola"
+
+# print(name is name2)
+# print(name is not name2)
+
+# print(id(name))
+# print(id(name2))
