00d12f19c3733fb6f1202e1e387c9db283a845c4
alopay <phikatravel@gmail.com>
2023-04-03 13:10:34 +0100

    recap3string git commit -m recap3string

---
+++ recap3.py
@@ -0,0 +1,23 @@
+# Strings
+
+fname = "Hakeem"
+lname = "Alopay"
+
+# Addition of strings
+fullname = fname + " " + lname # Good for concatenation
+# fullname1 = fname, lname # Bad for Concatentaion
+
+print(fullname)
+# print(fullname1)
+
+
+# Multiplication of strings
+f = "Hello "
+greetings = f * 3 + "!"
+print(greetings)
+
+# in operator
+name = "Bello"
+print('B' in name)
+print('o' in name)
+print('O' in name)
