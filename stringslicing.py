7ec12c6b9c10dea7c83b987c921a31134fa4182e
alopay <phikatravel@gmail.com>
2023-04-03 14:08:47 +0100

    recap3string git commit -m strslycing

---
+++ stringslicing.py
@@ -0,0 +1,27 @@
+# Strings
+
+# String Indexing
+# in programming, counting starts from 0
+# In maths, counting starts from 1
+animal = "Elephant"
+
+# print(animal)
+# print(animal[0]) # This prints out the first character "E"
+# print(animal[2])
+# print(animal[-8])
+# print(animal[-1]) # -1 prints the last character
+
+
+# slicing
+print(animal[2:5])
+# print(animal[0:3])
+# # Print "ant"
+# print(animal[5:8])
+# print(animal[5:]) # : counts from the position to the end of index
+# print(animal[:5]) # : counts from the beginning
+
+# wild_animal = animal[:] # : counts from the beginning to the end
+# print(wild_animal)
+
+
+print(animal[::4]) # second column while slicing is used for step or striding
