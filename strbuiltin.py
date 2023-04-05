c7b156635e84cd6e721971bba5277498246e7c6b
alopay <phikatravel@gmail.com>
2023-04-05 16:44:46 +0100

    strbuiltin.py

---
+++ strbuiltin.py
@@ -0,0 +1,26 @@
+# Strings built in methods
+
+name = "bello"
+full_name = "bello abdul hakeem"
+# capitalize() - Capitalize the first character in targetted strings
+
+print(full_name)
+print(full_name.capitalize())
+
+# title() - Capitalize first character for every word in targetted strings
+
+print(full_name.title())
+
+
+# upper() - Capitalize every character in the targetted strings
+print(full_name.upper())
+
+name2 = "MR ALOPAY"
+
+print(name2.title())
+
+# lower() - lower case every character in the targetted strings
+
+print(name2.lower())
+
+print(name2[3:].lower())
