d820419a54276041f08814eb3fea8d1c1caceb8a
alopay <phikatravel@gmail.com>
2023-04-10 13:24:33 +0100

    listoperator.py

---
+++ listoperators.py
@@ -0,0 +1,33 @@
+# operators in list
+
+# in operators can also be used on lists
+
+vegetables = ["Tomato", "Carrot", "Onion", "Lettuce"]
+
+# print("pepper" not in vegetables)
+
+# len() get the length of the list
+
+# print(len(vegetables))
+
+# print(f'The items present in vegetable list are {vegetables}')
+# print(f'The length of vegetables is {len(vegetables)}')
+
+
+# min() gets the lowest item on the list.
+# print(min(vegetables))
+
+random_item = ["car", "Cabe", "Cabi", "cab"]
+
+random_numbers = [2, 9, 13, 1, 99]
+random_check = [True, False] # True is repr mathematically as 1, False means 0
+
+print(min(random_item))
+print(min(random_numbers))
+print(min(random_check))
+
+# max() get the highest item on the list
+print(max(random_item))
+print(max(random_numbers))
+print(max(random_check))
+
