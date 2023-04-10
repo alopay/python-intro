ae16b65b36765d4ec0c3e8dc294f5aa278af74e3
alopay <phikatravel@gmail.com>
2023-04-10 12:58:25 +0100

    listintroduction.py

---
+++ listintroduction.py
@@ -0,0 +1,38 @@
+# List
+
+# str, int, bool
+
+# student1_name = "Ola"
+# student1_age = 32
+# student1_ispresent = True
+
+# List
+student1 = ["Ola", 32, True]
+student2 = [36, False, "Bola"]
+print(student1)
+print(student2)
+
+# index()
+
+name = "Ola"
+# print(name[0:2])
+print(student2[-1])
+
+number1 = [1, 2, 3, 4]
+number2 = [4, 3, 2, 1]
+number3 = [1, 2, 3, 4]
+
+# print(number1 == number2)
+# print(number1 == number3)
+
+list_numbered = ["Alopay", 40, True, 30000.22, number1]
+print(list_numbered)
+
+list_numbered = ["Pay", 31, False, 500000.44, number2]
+print(list_numbered)
+"""
+  1. lists are ordered
+  2. lists can contain arbitrary objects
+  3. lists can be accessed by index position
+  4. lists are mutable.
+"""
