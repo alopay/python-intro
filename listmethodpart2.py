ea2c81aa41029b049a5a07eaff39ef0c8c0420c1
alopay <phikatravel@gmail.com>
2023-04-11 13:14:32 +0100

    listmethod2.py

---
+++ listmethodpart2.py
@@ -0,0 +1,14 @@
+# lists methods
+
+fruits = ['orange', 'cucumber', 'date', 'banana']
+
+# .pop() method remove an item from a list using it's index position
+
+# fruits.pop(-1)
+print(fruits)
+
+# fruits[2:3] = []
+# print(fruits)
+
+# fruits[2] = []
+# print(fruits)
