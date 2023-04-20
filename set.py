b854e9a93d4b13ca2479ee07fbf3539b75658cea
alopay <phikatravel@gmail.com>
2023-04-20 13:34:54 +0100

    set.py

---
+++ set.py
@@ -0,0 +1,32 @@
+# sets is {}
+
+# sets cannot be ordered
+# sets are unique
+
+# bool values are represented as 1 and 0. True means 1, False means 0.
+
+numbers = {1, 0, "testme", "Try me", True, 3, 4, 5, 7, False}
+
+print(numbers)
+print(numbers)
+
+
+number2 = {12, 0, 19, 22, 300, 1, 1}
+number3 = [12, 0, 19, 22, 300, 1, 1]
+print(number2)
+print(number3)
+
+brands = {'pepsi', 'kfc', 'amazon', 'jumia', 'konga'}
+
+print(brands)
+
+drink = "pepsi"
+
+drink_list = list(drink)
+
+print(drink_list)
+
+set_drink = set(drink_list)
+
+print(set_drink)
+
