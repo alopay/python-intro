b48ef4fd03199ad6ef326dfe38819089029a230d
alopay <phikatravel@gmail.com>
2023-05-31 16:41:15 +0100

    squarearea.py

---
+++ squarearea.py
@@ -0,0 +1,17 @@
+print("""
+  The aim of this code is to calculate the areas of rectangles, squares and circle
+""")
+
+# inputs are always str
+
+# formular for square = l * l or l ** 2
+
+length = int(input('Enter the length of the square: '))
+
+
+area = length ** 2
+
+print(
+  f'The area of a square of length {length} is {area}.'
+)
+
