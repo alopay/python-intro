7ddc7772621d635b381d523a77a45cdb04957d24
alopay <phikatravel@gmail.com>
2023-06-07 12:31:56 +0100

    ifchallenge1.py

---
+++ ifchallenge1.py
@@ -0,0 +1,8 @@
+# Write a program that checks if a number is positive or negative. If it's positive, print "Positive number." Otherwise, print "Negative number."
+
+user_input = int(input('Enter a number here: '))
+
+if user_input >= 0:
+  print("Positive Number")
+else:
+  print("Negative Number")
